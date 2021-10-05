01ca1f2b8ee7dc4b8ea7bfe967b44c72a9cec3dd
murokozi <87635601+murokozi@users.noreply.github.com>
2021-10-05 14:42:05 +0200

    Update signupForm.php

--- signupForm.php
+++ signupForm.php
@@ -9,11 +9,11 @@
 <body>
 <h3>PHP and mysql web app</h3>
 <form action="signup.php" method="POST">
-FirstName: <input type="text"name="FirstName"><br><br>
-LastName:&nbsp<input type="text" name="LastName" width="300"height="300"><br><br>
-Email: &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input type="email"name="Email" width="300"height="300"><br><br>
+FirstName: <input type="text"name="FirstName" required><br><br>
+LastName:&nbsp;<input type="text" name="LastName" width="300"height="300"><br><br>
+Email: &nbsp;&nbs;p&nbsp;&nbsp;&nbsp;&nbsp; <input type="email"name="Email" width="300"height="300"required><br><br>
 UserName:<input type="text"name="UserName" width="300"height="300"><br><br>
-Password:&nbsp;&nbsp<input type="password"name="Password" width="300"height="300"><br>
+Password:&nbsp;&nbsp;<input type="password"name="Password" width="300"height="300"required><br>
 <button type="submit" name="signup">SignUp</button>
    <p> Alreadry have account?  <a href="loginForm.php">Log in</a> </p>
 </form>

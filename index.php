6f4df90049669f334af048a0fe131fdc080d7ea5
murokozi <jackm@DESKTOP-UOO39EN.(none)>
2021-10-07 09:41:56 +0200

    adding index file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+if(!isset($_SESSION['k1'])){
+	header('location:loginform.php');
+}?><!DOCTYPE html>
+<html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width , initial-scale=1.0">
+<title>Welcome</title>
+<p><a href="logout.php.php">logout.php</a> </p>
+</head>
+<body>
+	
+	<p>welcome to our website</p>
+
+</body>
+</html>
